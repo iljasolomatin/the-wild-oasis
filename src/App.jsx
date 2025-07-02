2779846f546a67cb1eefed2ddaf4f0c601acc5e2
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-02 15:36:59 +0100

    Add styled-components and refactor UI components
    Integrated styled-components for styling, replacing the global CSS file with a GlobalStyle.js using createGlobalStyle. Refactored App.jsx to use styled components, added new styled Button and Input components, and updated index.html with custom fonts. Updated dependencies to include styled-components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
