659d1697366855571f00f0ec5051585dd788e5dd
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-03 19:37:46 +0100

    Integrate Supabase and React Query for cabins data
    Added Supabase client and API service for fetching cabins. Integrated @tanstack/react-query and devtools, and refactored the cabins page to use a new CabinTable component that fetches data using React Query.

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://mddfcemyrvezedslikos.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kZGZjZW15cnZlemVkc2xpa29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDcxMzQsImV4cCI6MjA2NzEyMzEzNH0.gHT4T1ridkTJ2LLOr8aoSTenKa88qYNpH9bdYJ0rPqw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
