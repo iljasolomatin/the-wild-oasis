a207fe5edd572efe3cdfcbadb393adef4354800f
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-15 15:07:51 +0100

    Refactor cabin modals and improve delete confirmation
    Refactored AddCabin and CabinRow to use the Modal component for editing and deleting cabins, replacing local state with modal windows. Updated ConfirmDelete to support closing the modal and improved button handlers. Cleaned up unused imports and simplified modal usage.

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,25 +1,19 @@
 import Button from "../../ui/Button.jsx";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import Modal from "../../ui/Modal.jsx";
-import CabinTable from "./CabinTable-v1.jsx";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
