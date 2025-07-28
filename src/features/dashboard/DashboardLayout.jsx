c50aa7143436af18b333ceded126d22ba3be1bea
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-28 17:58:58 +0100

    Add dashboard stats and recent bookings/stays hooks
    Introduced Stats component to display key metrics on the dashboard. Added useRecentBookings and useRecentStays hooks for fetching recent booking and stay data. Updated DashboardLayout to use these hooks and show loading spinner while data is loading. Cleaned up apiBookings.js by removing commented code.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings.js";
+import Spinner from "../../ui/Spinner.jsx";
+import { useRecentStays } from "./useRecentStays.js";
+import Stats from "./Stats.jsx";
+import { useCabins } from "../cabins/useCabins.js";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,10 +13,26 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isPending: isPending1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isPending: isPending2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isPending3 } = useCabins();
+
+  if (isPending1 || isPending2 || isPending3) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Statistics</div>
-      <div>Today's activity</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today&apos;s activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
     </StyledDashboardLayout>
---
+++ src/features/dashboard/Stats.jsx
@@ -0,0 +1,56 @@
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import Stat from "./Stat.jsx";
+import { formatCurrency } from "../../utils/helpers.js";
+import SpinnerMini from "../../ui/SpinnerMini.jsx";
+
+function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
+  // 1.
+  const numBookings = bookings?.length;
+
+  // 2.
+  const sales = bookings?.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // 3.
+  const checkins = confirmedStays?.length;
+
+  // 4.
+  const occupation =
+    confirmedStays?.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings || <SpinnerMini />}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales) || <SpinnerMini />}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins || <SpinnerMini />}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupation * 100) + "%" || <SpinnerMini />}
+      />
+    </>
+  );
+}
+
+export default Stats;
