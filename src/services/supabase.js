87df14e506e5c6344a00192b5f0bdf6dcf858217
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-29 17:48:48 +0100

    Put anon key back into supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { SUPABASE_KEY } from "./keys.js";
 
 export const supabaseUrl = "https://mddfcemyrvezedslikos.supabase.co";
-const supabaseKey = SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kZGZjZW15cnZlemVkc2xpa29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDcxMzQsImV4cCI6MjA2NzEyMzEzNH0.gHT4T1ridkTJ2LLOr8aoSTenKa88qYNpH9bdYJ0rPqw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
