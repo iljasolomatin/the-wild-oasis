884b5c304e83822631066289786d04ce15b0a702
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-07-29 15:18:39 +0100

    Add DurationChart and enhance SalesChart heading
    Introduced the DurationChart component to display stay duration summaries using a pie chart and integrated it into the DashboardLayout. Also updated the SalesChart heading to show the date range for sales data, improving dashboard clarity.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays.js";
 import Stats from "./Stats.jsx";
 import { useCabins } from "../cabins/useCabins.js";
 import SalesChart from "./SalesChart.jsx";
+import DurationChart from "./DurationChart.jsx";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today&apos;s activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
